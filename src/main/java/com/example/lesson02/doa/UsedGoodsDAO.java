24e85183c414c9a34299f089acc42270981d84f3
DESKTOP-P0MDT5B\user <aaaa@naver.com>
2022-06-23 18:47:12 +0900

    db설정예제

---
+++ src/main/java/com/example/lesson02/doa/UsedGoodsDAO.java
@@ -0,0 +1,19 @@
+package com.example.lesson02.doa;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.lesson02.model.UsedGoods;
+
+@Repository
+public interface UsedGoodsDAO {
+	
+	public List<UsedGoods>selectUsedGoodsList();
+
+	
+		
+	
+	
+
+}
