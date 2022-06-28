cf2a5dd6f5f01e3815598a40f5aef1ef4388f08c
DESKTOP-P0MDT5B\user <aaaa@naver.com>
2022-06-28 18:49:19 +0900

    jsp 유저정보 셀렉트 인설트

---
+++ src/main/java/com/example/lesson04/bo/AddUserBO.java
@@ -0,0 +1,21 @@
+package com.example.lesson04.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.lesson04.dao.AddUserDAO;
+import com.example.lesson04.model.AddUser;
+@Service
+public class AddUserBO {
+	
+     @Autowired
+     private AddUserDAO addUserDAO;
+	public void addUser(String name , String yyyymmdd, String email, String introduce ) {
+		addUserDAO.insertaddUser(name, yyyymmdd, email, introduce);
+	}
+	public AddUser getLastNewUser() {
+		return addUserDAO.selectLastNewUser();
+		
+	}
+
+}
---
+++ src/main/java/com/example/lesson04/dao/AddUserDAO.java
@@ -0,0 +1,18 @@
+package com.example.lesson04.dao;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.example.lesson04.model.AddUser;
+
+@Repository
+public interface AddUserDAO {
+	public int insertaddUser(
+			@Param("name") String name, 
+			@Param("yyyymmdd") String yyyymmdd, 
+			@Param("email") String email, 
+			@Param("introduce") String introduce );
+	
+	public AddUser selectLastNewUser();
+
+}
