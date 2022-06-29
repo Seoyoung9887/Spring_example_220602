68c707a7f9aa5b916f8b7b2adc7abb04314aec47
DESKTOP-P0MDT5B\user <aaaa@naver.com>
2022-06-29 18:46:08 +0900

    학생등록 예제

---
+++ src/main/java/com/example/lesson04/bo/StudentBO.java
@@ -0,0 +1,12 @@
+package com.example.lesson04.bo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentBO {
+	
+	public void getStudent(String name, String phoneNumber, String email,String dreamJob ) {
+		
+	}
+
+}
