3e78c458f99d97687dbfbfd06dfddf72d8ef7f15
DESKTOP-P0MDT5B\user <aaaa@naver.com>
2022-07-01 18:53:24 +0900

    jsp ex 예제20220701

--- src/main/java/com/example/lesson05/Lesson05Controller.java
+++ src/main/java/com/example/lesson05/Lesson05Controller.java
@@ -1,6 +1,13 @@
 package com.example.lesson05;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,6 +17,41 @@ public class Lesson05Controller {
 	@RequestMapping("/lesson05/ex01")
 	public String ex01() {
 		return"lesson05/ex01";
+	}
+	
+	@RequestMapping("lesson05/ex02")
+	public String ex02(Model model) {
+		List<String> fruits = new ArrayList<>();
+		fruits.add("사과");
+		fruits.add("딸기");
+		fruits.add("배");
+		fruits.add("수박");
+		fruits.add("바나나");
+		
+		//List<map>
+		List<Map<String,Object>> users = new ArrayList<>();
+		Map<String,Object> user = new HashMap<>();
+		user.put("name", "서영빈");
+		user.put("age", 34);
+		user.put("hobby", "게임");
+		users.add(user);
+		
+		user = new HashMap<>(); 
+		user.put("name", "김유정");
+		user.put("age", 22);
+		user.put("hobby", "연기");
+		users.add(user);
+		
+		model.addAttribute("fruits", fruits);
+		model.addAttribute("users", users);
+		
+		return"lesson05/ex02";
+	}
+	@RequestMapping("/lesson05/ex03")
+	public String ex03(Model model) {
+		Date today = new Date();
+		model.addAttribute("today", today);
+		return "lesson05/ex03";
 	}
 
 }
